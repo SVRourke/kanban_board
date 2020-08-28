3f6940af4e029b0fc140884609992eab6127b561
SVR <samuel@rourke.tech>
2020-08-28 16:01:41 -0400

    Enabled sessions, set session key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+
+    # unsafe secret, used to simplify development
+    set :session_secret, 'Development'
+    # For better security, create an environement variable "SESSION_SECRET" set the value to SecureRandom.hex(64)
+    # set :session_secret, ENV.fetch('SESSION_SECRET')
+    
   end
 
   get "/" do
     erb :welcome
   end
+
+  # login
+  # signup
 
 end
