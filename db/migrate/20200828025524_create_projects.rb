dda32ae3e3bf759ed4e575bb8a05a1e4aa116b8f
SVR <samuel@rourke.tech>
2020-08-27 23:30:22 -0400

    created migrations for project, user, and task models

--- db/migrate/20200828025524_create_projects.rb
+++ db/migrate/20200828025524_create_projects.rb
@@ -1,7 +1,9 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-
+      t.string :title
+      t.string :description
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
--- db/migrate/20200828025837_create_tasks.rb
+++ db/migrate/20200828025837_create_tasks.rb
@@ -1,7 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-
+      t.belongs_to :list
+      t.belongs_to :user
+      t.string :content
+      t.string :doneness
       t.timestamps null: false
     end
   end
