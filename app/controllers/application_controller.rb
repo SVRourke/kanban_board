75a94efb35cea851dce92a4d675a6f7afdf932f1
SVR <samuel@rourke.tech>
2020-08-28 16:10:35 -0400

    added login to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,20 @@ class ApplicationController < Sinatra::Base
   end
 
   # login
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by_username(params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    else
+      redirect back
+    end
+  end
+
+  
   # signup
 
 end
