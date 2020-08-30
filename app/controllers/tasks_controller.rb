f98bd64e06e8150519f6ef58ea573be0526d1072
SVRourke <samuel@rourke.tech>
2020-08-30 12:57:53 -0400

    added a patch route to increment an item's progress state

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,5 @@
 class TasksController < ApplicationController
 
-  # GET: /tasks
-  get "/tasks" do
-    erb :"/tasks/index.html"
-  end
-
   # GET: /tasks/new
   get "/tasks/new" do
     erb :"/tasks/new.html"
@@ -12,12 +7,13 @@ class TasksController < ApplicationController
 
   # POST: /tasks
   post "/tasks" do
-    redirect "/tasks"
-  end
-
-  # GET: /tasks/5
-  get "/tasks/:id" do
-    erb :"/tasks/show.html"
+    @task = Task.create(
+      :user => current_user,
+      :project => Project.find_by_id(params[:project_id]),
+      :content => params[:content],
+      :doneness => 0
+    )
+    redirect "/projects/#{params[:project_id]}"
   end
 
   # GET: /tasks/5/edit
@@ -29,9 +25,31 @@ class TasksController < ApplicationController
   patch "/tasks/:id" do
     redirect "/tasks/:id"
   end
-
-  # DELETE: /tasks/5/delete
-  delete "/tasks/:id/delete" do
-    redirect "/tasks"
+# PATCH: /tasks/5
+patch "/tasks/:id/move-up" do
+  @task = Task.find_by_id(params[:id])
+  # binding.pry
+  current_stage = @task.doneness.to_i
+  if @task.doneness.to_i < 2
+    @task.update(:doneness => current_stage += 1)
+    redirect back
+  else
+    redirect back
   end
 end
+  # DELETE: /tasks/5/delete
+  delete "/tasks/:id/delete" do
+    @task = Task.find_by_id(params[:id])
+    @task.destroy
+    redirect back
+  end
+end
+# GET: /tasks
+  # get "/tasks" do
+  #   erb :"/tasks/index.html"
+  # end
+
+# GET: /tasks/5
+  # get "/tasks/:id" do
+  #   erb :"/tasks/show.html"
+  # end
