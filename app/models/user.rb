8085429a2026ab049c6368ab1ee9ee911b7114a4
SVRourke <samuelvanrourke@gmail.com>
2020-09-23 14:22:14 -0400

    added activerecord validations for the user model: username, email, and password. with accompanying error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
     has_secure_password
-    
+    validates :username, uniqueness: {message: "Username Already Taken!"}
+    validates :email, uniqueness: {message: "Email Already In Use!"}
+    validates :password, length: {minimum: 5, message: "Password Must Be Longer Than 5 Characters!"}
+
     has_many :projects
     has_many :tasks, through: :projects
 end
