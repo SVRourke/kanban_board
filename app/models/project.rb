12cae26d5b6a8bda4fdb30d9d6454a0fb76a75d0
SVRourke <samuelvanrourke@gmail.com>
2020-09-23 14:32:21 -0400

    added title and description validations to project model with accompanying error messages

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
+    validates :title, uniqueness: {message: "Project %{value} Already Exists!"}
+    validates :title, presence: {message: "Project Must Have A Title!"}
+    validates :description, presence: {message: "Project Must Have A Description!"}
+
     belongs_to :user
     has_many :tasks
 end
