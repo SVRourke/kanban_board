4c333ad611951e36a63daebf1b4c37728fc4b98f
SVRourke <samuelvanrourke@gmail.com>
2020-09-24 09:38:59 -0400

    added activrecord validations to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
+    validates :content, uniqueness: {message: "Task already in project!"}
+    validates :content, presence: {message: "Cannot Save Blank Task!"}
+
     belongs_to :user
     belongs_to :project
 end
