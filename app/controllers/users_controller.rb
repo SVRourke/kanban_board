1a72241843130528f575ffc9cbf38a93010cfcd3
SVRourke <samuelvanrourke@gmail.com>
2020-09-27 10:16:06 -0400

    moved the route to view projects to the project controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
-
-  # GET: /users/5
-  get "/users/:id" do
-    unauthorized_redirect
-    @user = current_user
-    erb :"/users/show.html"
-  end
   
   # DELETE: /users/5/delete
   delete "/users/:id/delete" do
