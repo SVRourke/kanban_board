39f73a85a22efd622d9f1dc12503bbec9c0c13ea
SVRourke <samuelvanrourke@gmail.com>
2020-09-27 10:05:03 -0400

    removed unneeded code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,6 @@ class UsersController < ApplicationController
     erb :"/users/show.html"
   end
   
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    unauthorized_redirect
-    erb :"/users/edit.html"
-  end
-
-  
-  # PATCH: /users/5
-  patch "/users/:id" do
-    unauthorized_redirect
-    redirect "/users/:id"
-  end
-  
   # DELETE: /users/5/delete
   delete "/users/:id/delete" do
     unauthorized_redirect
@@ -29,18 +16,3 @@ class UsersController < ApplicationController
     redirect "/signup"
   end
 end
-
-# GET: /users
-# get "/users" do
-#   erb :"/users/index.html"
-# end
-
-# GET: /users/new
-# get "/users/new" do
-#   erb :"/users/new.html"
-# end
-
-# POST: /users
-# post "/users" do
-#   redirect "/users"
-# end
